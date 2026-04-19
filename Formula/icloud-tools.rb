fabee33b69c7c64578967887af2381913fb5bf72
Jonas Frost <j@duplo.org>
2026-04-19 22:37:18 +0700

    Bump icloud-tools to v0.6.5 -- fix lying output on mv/cp

--- Formula/icloud-tools.rb
+++ Formula/icloud-tools.rb
@@ -1,8 +1,8 @@
 class IcloudTools < Formula
   desc "CLI for managing iCloud Drive files (replacement for brctl download/evict)"
   homepage "https://github.com/icanhasjonas/icloud-tools"
-  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.6.4.tar.gz"
-  sha256 "1e6051da0bc55fedca0be316fb9c4fccd3162eb3491c40e8382d7feec5f12e23"
+  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.6.5.tar.gz"
+  sha256 "675e6d448f950efb8e0ad8cde15c5ed86d14722c738632afcd91841f0298240a"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
