dda2ccbd06a212bb6d3cac576fbffcb29410aec3
Jonas Frost <j@duplo.org>
2026-04-19 20:26:52 +0700

    Bump icloud-tools to v0.5.1 -- POSIX arg parsing

--- Formula/icloud-tools.rb
+++ Formula/icloud-tools.rb
@@ -1,8 +1,8 @@
 class IcloudTools < Formula
   desc "CLI for managing iCloud Drive files (replacement for brctl download/evict)"
   homepage "https://github.com/icanhasjonas/icloud-tools"
-  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "2e7c4633a38d411d94b2f5b7e46d2386a462e8e6d10ed692b2478768110a1e6b"
+  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "9171154cfbfe401634282bd083a989cd006d9dad6477a53857ba354a4e961929"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
