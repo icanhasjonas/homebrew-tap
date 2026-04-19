37cd8adb9ba435407bb840c314e8567eedbf9275
Jonas Frost <j@duplo.org>
2026-04-19 22:31:35 +0700

    Bump icloud-tools to v0.6.4 -- fix silent error swallowing

--- Formula/icloud-tools.rb
+++ Formula/icloud-tools.rb
@@ -1,8 +1,8 @@
 class IcloudTools < Formula
   desc "CLI for managing iCloud Drive files (replacement for brctl download/evict)"
   homepage "https://github.com/icanhasjonas/icloud-tools"
-  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.6.3.tar.gz"
-  sha256 "a7f2cbd5bac09d358d7cd05960c3608a2cc859c36e629b63649b56c14b10bf81"
+  url "https://github.com/icanhasjonas/icloud-tools/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "1e6051da0bc55fedca0be316fb9c4fccd3162eb3491c40e8382d7feec5f12e23"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
